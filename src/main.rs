bffa0bc88e10f9b680722e48bd88b25b9650f705
TomArnaez <tomarnaez@gmail.com>
2023-03-06 00:23:32 +0000

    Add window

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use bevy::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+    // set antialiasing to 4
+    .insert_resource(Msaa { samples: 4})
+    // set WindowDescriptor Resource to change title and size
+    .add_plugins(DefaultPlugins.set(WindowPlugin {
+        window: WindowDescriptor { 
+            width: 1600.0,
+            height: 1600.0,
+            title: "Chessboard".to_string(),
+            ..default()   
+        },
+        ..default()
+    }))
+    .run();
 }
